beac9364f5807340550505cebd85d00350ea1f8f
pwirth <wirth@hallowelt.biz>
2019-10-22 10:46:55 +0200

    Init commit

---
+++ src/Hook/NamespaceManagerGetMetaFields/RegisterMetaField.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BlueSpice\CategoryCheck\Hook\NamespaceManagerGetMetaFields;
+
+use BlueSpice\NamespaceManager\Hook\NamespaceManagerGetMetaFields;
+
+class RegisterMetaField extends NamespaceManagerGetMetaFields {
+	
+	protected function doProcess() {
+		$this->metaFields[] = [
+			'name' => 'categorycheck',
+			'type' => 'boolean',
+			'label' => wfMessage( 'bs-categorycheck-nsm-label-categorycheck' )->plain(),
+			'filter' => [
+				'type' => 'boolean'
+			]
+		];
+
+		return true;
+	}
+
+}
